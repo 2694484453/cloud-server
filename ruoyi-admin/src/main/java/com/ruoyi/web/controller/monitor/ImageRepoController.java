1e196a18e0108abcff43567f58b9d68549fa1672
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-08-30 02:45:09 +0800

    init

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/ImageRepoController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/ImageRepoController.java
@@ -10,9 +10,11 @@ import cn.hutool.json.JSONUtil;
 import com.ruoyi.common.base.ExecResponse;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.utils.NerdCtlUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
@@ -31,8 +33,30 @@ import java.util.Map;
 public class ImageRepoController {
 
 
+    /**
+     * 支持类型查询
+     *
+     * @return r
+     */
+    @GetMapping("/typeList")
+    @ApiOperation("类型查询")
+    public AjaxResult typeList() {
+        String[] types = {};
+        types = ArrayUtil.append(types, "linux/amd64",
+                "linux/386",
+                "linux/arm64/v8");
+        return AjaxResult.success(types);
+    }
+
+    /**
+     * list
+     * @param namespace
+     * @return
+     */
     @GetMapping("/list")
-    public AjaxResult list() {
+    public AjaxResult list(@RequestParam(value = "namespace", required = false) String namespace) {
+        String[] init = {};
+
         ExecResponse execResponse = NerdCtlUtil.execFor("nerdctl", "images", "--format", "json");
         String data = execResponse.getSuccessMsg();
         Console.log("{}", data);
@@ -41,7 +65,7 @@ public class ImageRepoController {
             String line;
             while ((line = br.readLine()) != null) {
                 //System.out.println(line);
-                Map map = JSONUtil.toBean(line,Map.class);
+                Map map = JSONUtil.toBean(line, Map.class);
                 jsonArray.add(map);
             }
         } catch (IOException e) {
