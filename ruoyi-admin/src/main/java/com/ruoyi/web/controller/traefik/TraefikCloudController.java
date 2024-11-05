50727c51d90ef0cc90714259e42fc885e82dc2ea
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-11-06 01:50:08 +0800

    2024/11/06

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/traefik/TraefikCloudController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/traefik/TraefikCloudController.java
@@ -8,6 +8,8 @@ import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.utils.PageUtils;
 import io.fabric8.kubernetes.api.model.networking.v1.Ingress;
 import io.fabric8.kubernetes.api.model.networking.v1.IngressList;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,18 +22,31 @@ import java.util.List;
  **/
 @RestController
 @RequestMapping("/traefikCloud")
+@Api(tags = "traefik云原生版")
 public class TraefikCloudController {
 
+    /**
+     * 列表查询
+     *
+     * @return r
+     */
     @GetMapping("/list")
+    @ApiOperation(value = "列表查询")
     public AjaxResult list() {
         IngressList list = K8sUtil.createKClient().network().v1().ingresses().list();
         List<Ingress> ingressList = list.getItems();
         return AjaxResult.success(ingressList);
     }
 
+    /**
+     * 分页查询
+     *
+     * @return r
+     */
     @GetMapping("/page")
+    @ApiOperation(value = "分页查询")
     public TableDataInfo page() {
-        List ingressList = Convert.convert(List.class, list().get("data"));
+        List<Ingress> ingressList = Convert.toList(Ingress.class, list().get("data"));
         return PageUtils.toPage(ingressList);
     }
 }
