1c353b2ed0f144f413f0488b16c4da6907f68337
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-11-22 01:13:44 +0800

    2024/11/22

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nohup java -jar /home/my-server/ruoyi-admin/target/ruoyi-admin.jar output.log 2>&1 & tail -f output.log
+nohup java -jar /home/my-server/ruoyi-admin/target/ruoyi-admin.jar output.log 2>&1 & tail -f nohup.out
