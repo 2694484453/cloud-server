e20689d279e6798902e803e28e0a18b1673bf455
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-11-26 02:01:50 +0800

    2024/11/24

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/coredns/CoreDnsCloudController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/coredns/CoreDnsCloudController.java
@@ -1,11 +1,13 @@
 package com.ruoyi.web.controller.coredns;
 
 import cn.hutool.core.convert.Convert;
-import com.ruoyi.common.utils.K8sUtil;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.utils.PageUtils;
 import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author gaopuguang
@@ -24,27 +25,40 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RequestMapping("/corednsCloud")
 public class CoreDnsCloudController {
 
+    @Qualifier("KubernetesClient")
+    @Autowired
+    private KubernetesClient kubernetesClient;
+
+    /**
+     * 列表查询
+     *
+     * @return r
+     */
     @GetMapping("/list")
     public AjaxResult list() {
-        ConfigMap configMap = K8sUtil.createKClient().configMaps().inNamespace("kube-system").withName("coredns-custom").get();
+        ConfigMap configMap = kubernetesClient.configMaps().inNamespace("kube-system").withName("coredns-custom").get();
         Map<String, String> map = configMap.getData();
         List<HashMap<String, Object>> mapList = new ArrayList<>();
-        AtomicInteger i = new AtomicInteger();
         map.forEach((k, v) -> {
             mapList.add(new HashMap<String, Object>() {{
-                put("id",i);
                 put("key", k);
-                put("value",v);
+                put("value", v);
+                put("namespace", "kube-system");
+                put("name", "coredns-custom");
             }});
-            i.getAndIncrement();
         });
         return AjaxResult.success(mapList);
     }
 
+    /**
+     * 分页查询
+     *
+     * @return r
+     */
     @GetMapping("/page")
     public TableDataInfo page() {
         AjaxResult ajaxResult = list();
-        List mapList = Convert.convert(List.class, ajaxResult.get("data"));
+        List<?> mapList = Convert.toList(ajaxResult.get("data"));
         return PageUtils.toPage(mapList);
     }
 }
