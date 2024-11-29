37f62eed4b9e873796322cb12555b383de5a9bd3
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-11-30 04:08:13 +0800

    2024/11/24

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/git/GitController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/git/GitController.java
@@ -1,8 +1,10 @@
-package com.ruoyi.web.controller.build;
+package com.ruoyi.web.controller.git;
 
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
@@ -11,11 +13,14 @@ import com.ruoyi.common.utils.PageUtils;
 import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.web.controller.build.domain.Git;
 import com.ruoyi.web.controller.build.domain.GiteeRepo;
+import com.ruoyi.web.controller.git.domain.Gitee;
 import io.fabric8.kubernetes.api.model.batch.v1.Job;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +37,9 @@ import java.util.List;
 @RequestMapping("/git")
 @Api(tags = "git仓库")
 public class GitController {
+
+    @Autowired
+    private Gitee gitee;
 
     private static final String path = "/root/.my-server";
 
@@ -73,6 +81,23 @@ public class GitController {
         AjaxResult ajaxResult = list(type);
         List<?> list = Convert.toList(ajaxResult.get("data"));
         return PageUtils.toPage(list);
+    }
+
+    /**
+     * 获取token
+     * @param code code
+     * @return r
+     */
+    @PostMapping("/token")
+    @ApiOperation(value = "获取token")
+    public AjaxResult token(@RequestParam(value = "code") String code) {
+        HttpResponse response = HttpUtil.createPost(gitee.getApi())
+                .form("client_id", gitee.getClient_id())
+                .form("grant_type", gitee.getGrant_type())
+                .form("redirect_uri", gitee.getRedirect_uri())
+                .body(JSONUtil.toJsonStr(gitee.getClient_secret()))
+                .execute(false);
+        return AjaxResult.success("请求成功", response.body());
     }
 
 
