10504e8ef6adca840f80dd64aff11a8700605b6f
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-12-02 23:57:18 +0800

    2024/12/01

--- k8s/install.sh
+++ k8s/install.sh
@@ -1,1 +1,4 @@
-helm install kubernetes-dashboard -n kubernetes-dashboard ./kubernetes-dashboard-7.5.0.tgz --kubeconfig ./k3s.gpg123.vip --create-namespace
+helm install kubernetes-dashboard -n kubernetes-dashboard ./kubernetes-dashboard-7.5.0.tgz --kubeconfig ./k3s.yaml --create-namespace
+
+
+helm install kube-prometheus-stack -n monitoring ./kube-prometheus-stack-66.3.0.tgz --kubeconfig ./k3s.yaml --create-namespace
