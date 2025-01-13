c1c8e3ad6f76683f2b743b0a90b4d2edfb8e7d4d
gaopuguang <2694484453@qq.com>
2025-01-13 02:12:25 +0000

    1
    Signed-off-by: gaopuguang <2694484453@qq.com>

---
+++ cloud-server-framework/src/main/java/vip/gpg123/framework/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.zcrjjh.framework.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis-plush分页器
+ *
+ * @author yys
+ * @version 1.0
+ * @description:
+ * @date 2023/8/18 10:03
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        // 溢出总页数后是否进行处理
+        paginationInnerInterceptor.setOverflow(true);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
+}
