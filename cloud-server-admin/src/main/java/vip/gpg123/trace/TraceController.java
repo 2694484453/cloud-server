9b709f1c69506628f4416a7732cdf95578d89646
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-02-07 01:52:25 +0800

    2025/02/03 git pull

---
+++ cloud-server-admin/src/main/java/vip/gpg123/trace/TraceController.java
@@ -0,0 +1,71 @@
+package vip.gpg123.trace;
+
+import cn.hutool.core.convert.Convert;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vip.gpg123.common.core.domain.AjaxResult;
+import vip.gpg123.common.core.page.TableDataInfo;
+import vip.gpg123.common.utils.PageUtils;
+
+import java.util.List;
+
+/**
+ * @author gaopuguang
+ * @date 2025/2/7 1:42
+ **/
+@RestController
+@RequestMapping("/trace")
+@Api(tags = "【trace】链路追踪")
+public class TraceController {
+
+    @Value("${trace.api}")
+    private String api;
+
+    /**
+     * 分页
+     *
+     * @return r
+     */
+    @GetMapping("/page")
+    @ApiOperation(value = "分页查询")
+    public TableDataInfo page() {
+        List<?> list = getServiceList();
+        return PageUtils.toPage(list);
+    }
+
+    /**
+     * 列表查询
+     *
+     * @return r
+     */
+    @GetMapping("/list")
+    @ApiOperation(value = "列表查询")
+    public AjaxResult list() {
+        List<?> list = getServiceList();
+        return AjaxResult.success(list);
+    }
+
+    /**
+     * 查询服务
+     *
+     * @return r
+     */
+    private List<?> getServiceList() {
+        HttpResponse httpResponse = HttpUtil.createGet(api + "/services")
+                .timeout(10000)
+                .setConnectionTimeout(10000)
+                .execute();
+        JSONObject jsonObject = JSONUtil.parseObj(httpResponse.body());
+        JSONArray jsonArray = JSONUtil.parseArray(jsonObject.get("data"));
+        return Convert.toList(jsonArray);
+    }
+}
