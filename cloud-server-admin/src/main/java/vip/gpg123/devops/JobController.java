8ccce6ca61c7a78fc357785e2caa2af7e26315d0
‘gaopuguang_zz’ <gaopg09@chinatelecom.cn>
2025-02-11 14:43:21 +0800

    2025/02/07 info

--- cloud-server-admin/src/main/java/vip/gpg123/devops/JobController.java
+++ cloud-server-admin/src/main/java/vip/gpg123/devops/JobController.java
@@ -1,9 +1,12 @@
 package vip.gpg123.devops;
 
 import cn.hutool.core.convert.Convert;
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.ObjectUtil;
 import io.fabric8.kubernetes.api.model.batch.v1.Job;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.PodResource;
+import io.fabric8.kubernetes.client.dsl.ScalableResource;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +16,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import vip.gpg123.common.core.domain.AjaxResult;
 import vip.gpg123.common.core.page.TableDataInfo;
 import vip.gpg123.common.utils.K8sUtil;
 import vip.gpg123.common.utils.PageUtils;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -105,6 +111,7 @@ public class JobController {
 
     /**
      * 获取日志
+     *
      * @param name 名称
      * @return r
      */
@@ -113,5 +120,38 @@ public class JobController {
     public AjaxResult log(@RequestParam("name") String name) {
         String logs = client.batch().v1().jobs().inNamespace("default").withName(name).getLog(true);
         return AjaxResult.success(logs);
+    }
+
+    /**
+     * 获取日志
+     *
+     * @param jobName   jn
+     * @param nameSpace ns
+     * @return r
+     */
+    @GetMapping("/podLogs")
+    @ApiOperation(value = "日志")
+    public SseEmitter podLogs(@RequestParam(value = "jobName") String jobName,
+                              @RequestParam(value = "nameSpace") String nameSpace) {
+        // 用于创建一个 SSE 连接对象
+        SseEmitter emitter = new SseEmitter();
+        ThreadUtil.execute(() -> {
+            try {
+                ScalableResource<Job> job = client.batch().v1().jobs().inNamespace(nameSpace).withName(jobName);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(job.watchLog().getOutput()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    emitter.send(line);
+                }
+                // 数据发送完成后，关闭连接
+                emitter.complete();
+            } catch (Exception e) {
+                // 发生错误时，关闭连接并报错
+                emitter.completeWithError(e);
+            } finally {
+
+            }
+        });
+        return emitter;
     }
 }
