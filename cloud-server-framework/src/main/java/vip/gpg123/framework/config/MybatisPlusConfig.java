badc7ffa57bc98a031a7f5ba475fe2a283843e74
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-02-16 01:57:03 +0800

    2025/02/16 调整

--- cloud-server-framework/src/main/java/vip/gpg123/framework/config/MybatisPlusConfig.java
+++ cloud-server-framework/src/main/java/vip/gpg123/framework/config/MybatisPlusConfig.java
@@ -3,8 +3,13 @@ package vip.gpg123.framework.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.github.pagehelper.PageInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
 
 /**
  * mybatis-plush分页器
@@ -15,7 +20,10 @@ import org.springframework.context.annotation.Configuration;
  * @date 2023/8/18 10:03
  */
 @Configuration
+@MapperScan(basePackages = {"vip.gpg123.**.mapper"})
+@EnableTransactionManagement
 public class MybatisPlusConfig {
+
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
@@ -26,5 +34,15 @@ public class MybatisPlusConfig {
         paginationInnerInterceptor.setDbType(DbType.MYSQL);
         interceptor.addInnerInterceptor(paginationInnerInterceptor);
         return interceptor;
+    }
+
+    @Bean
+    PageInterceptor pageInterceptor(){
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        properties.setProperty("helperDialect", "mysql");
+        // 由此可进入源码，
+        pageInterceptor.setProperties(properties);
+        return pageInterceptor;
     }
 }
