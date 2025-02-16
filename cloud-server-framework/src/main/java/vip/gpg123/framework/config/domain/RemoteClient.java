d91b69afaf0da6b18b419a184f1259d43a4b8b09
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-02-17 01:33:21 +0800

    2025/02/16 调整

---
+++ cloud-server-framework/src/main/java/vip/gpg123/framework/config/domain/RemoteClient.java
@@ -0,0 +1,23 @@
+package vip.gpg123.framework.config.domain;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author gaopuguang
+ * @date 2025/2/16 20:20
+ **/
+@Data
+@Component
+@ConfigurationProperties(prefix = "remote")
+public class RemoteClient {
+
+    private String host;
+
+    private String port;
+
+    private String userName;
+
+    private String passWord;
+}
