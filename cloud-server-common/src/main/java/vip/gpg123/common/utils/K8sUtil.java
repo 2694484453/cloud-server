767a0094899cd14770896c334e5ca025c3fbaddf
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-02-28 23:27:17 +0800

    2025/02/22 集成邮件

--- cloud-server-common/src/main/java/vip/gpg123/common/utils/K8sUtil.java
+++ cloud-server-common/src/main/java/vip/gpg123/common/utils/K8sUtil.java
@@ -15,13 +15,26 @@ import java.nio.charset.StandardCharsets;
  **/
 public class K8sUtil {
 
+    /**
+     * 配置文件地址
+     */
+    private static final String configFile = SystemUtil.getOsInfo().isWindows() ? "C:/Users/" + SystemUtil.getUserInfo().getName() + "/.kube/config" : "/root/.kube/config";
+
 
     /**
      * k8s客户端
      * @return r
      */
     public static KubernetesClient createKClient() {
-   
+        try {
+            String content = FileUtil.readString(configFile, StandardCharsets.UTF_8);
+            Config config = Config.fromKubeconfig(content);
+            return new KubernetesClientBuilder()
+                    .withConfig(config)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -29,7 +42,8 @@ public class K8sUtil {
      * @return r
      */
     public static OpenShiftClient createOClient() {
-
+        KubernetesClient kubernetesClient = createKClient();
+       return kubernetesClient.adapt(OpenShiftClient.class);
     }
 
     public static String defaultConfigFilePath() {
