dfa2ceb9977afca926135aa62044bac34e978ab4
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-03-02 16:53:28 +0800

    2025/03/02 nacos

---
+++ cloud-server-admin/src/main/java/vip/gpg123/discovery/NacosOverViewController.java
@@ -0,0 +1,40 @@
+package vip.gpg123.discovery;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vip.gpg123.common.core.domain.AjaxResult;
+import vip.gpg123.framework.config.domain.NacosClient;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author gaopuguang
+ * @date 2025/3/2 16:50
+ **/
+@RestController
+@RequestMapping("/discovery/nacos")
+@Api(tags = "【discovery】nacos服务概览")
+public class NacosOverViewController {
+
+    @Autowired
+    private NacosClient nacosClient;
+
+    /**
+     * 概览
+     * @return r
+     */
+    @GetMapping("/overView")
+    private AjaxResult overView() {
+        List<?> namespaceList = nacosClient.namespaceList();
+        List<?> serviceList = nacosClient.serviceList();
+        Map<String, Object> result = new HashMap<>();
+        result.put("namespaceCount", namespaceList.size());
+        result.put("serviceCount", serviceList.size());
+        return AjaxResult.success(result);
+    }
+}
