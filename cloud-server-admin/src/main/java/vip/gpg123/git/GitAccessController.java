22cb71ccacbfaebe152e4a51b53d6bde89e6ba7d
gaopuguang <2694484453@qq.com>
2025-04-30 00:52:45 +0800

    2025/04/29 helmapp

---
+++ cloud-server-admin/src/main/java/vip/gpg123/git/GitAccessController.java
@@ -0,0 +1,188 @@
+package vip.gpg123.git;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import vip.gpg123.common.core.controller.BaseController;
+import vip.gpg123.common.core.domain.AjaxResult;
+import vip.gpg123.common.core.page.TableDataInfo;
+import vip.gpg123.common.core.page.TableSupport;
+import vip.gpg123.common.utils.PageUtils;
+import vip.gpg123.common.utils.SecurityUtils;
+import vip.gpg123.domain.Email;
+import vip.gpg123.framework.manager.AsyncManager;
+import vip.gpg123.git.domain.GitAccess;
+import vip.gpg123.git.service.GitAccessService;
+
+import java.util.List;
+import java.util.TimerTask;
+
+/**
+ * @author gpg
+ * @date 2020/6/29
+ * @description Git仓库认证管理
+ */
+@RestController
+@RequestMapping("/gitAccess")
+@Api(value = "Git仓库认证管理")
+public class GitAccessController extends BaseController {
+
+    @Autowired
+    private GitAccessService gitAccessService;
+
+
+    /**
+     * 列表查询
+     *
+     * @param name 名称
+     * @param type 类型
+     * @return r
+     */
+    @GetMapping("/list")
+    @ApiOperation(value = "列表查询")
+    public AjaxResult list(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam(value = "type", required = false) String type) {
+        List<GitAccess> gitAccessList = gitAccessService.list(new LambdaQueryWrapper<GitAccess>()
+                .eq(StrUtil.isNotBlank(type), GitAccess::getType, type)
+                .like(StrUtil.isNotBlank(name), GitAccess::getName, name)
+                .eq(GitAccess::getCreateBy, getUsername())
+                .orderByDesc(GitAccess::getCreateTime)
+        );
+        return AjaxResult.success(gitAccessList);
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param name 名称
+     * @param type 类型
+     * @return r
+     */
+    @GetMapping("/page")
+    @ApiOperation(value = "分页查询")
+    public TableDataInfo page(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "type", required = false) String type) {
+        IPage<GitAccess> page = new Page<>(TableSupport.buildPageRequest().getPageNum(), TableSupport.buildPageRequest().getPageSize());
+        page = gitAccessService.page(page, new LambdaQueryWrapper<GitAccess>()
+                .eq(StrUtil.isNotBlank(type), GitAccess::getType, type)
+                .like(StrUtil.isNotBlank(name), GitAccess::getName, name)
+                .eq(GitAccess::getCreateBy, getUsername()));
+        return PageUtils.toPageByIPage(page);
+    }
+
+    /**
+     * 详情查询
+     *
+     * @param id id
+     * @return r
+     */
+    @GetMapping("/info")
+    @ApiOperation(value = "详情查询")
+    public AjaxResult info(@RequestParam(value = "id") String id) {
+        GitAccess gitAccess = gitAccessService.getById(id);
+        return AjaxResult.success(gitAccess);
+    }
+
+    /**
+     * 添加
+     *
+     * @param gitAccess gitAccess
+     * @return r
+     */
+    @PostMapping("/add")
+    @ApiOperation(value = "添加")
+    public AjaxResult add(@RequestBody GitAccess gitAccess) {
+        // 获取邮箱
+        String userEmail = SecurityUtils.getLoginUser().getUser().getEmail();
+        // 获取用户名
+        String userName = SecurityUtils.getLoginUser().getUser().getUserName();
+        gitAccess.setCreateBy(getUsername());
+        gitAccess.setCreateTime(DateUtil.date());
+        boolean isSuccess = gitAccessService.save(gitAccess);
+        // 异步消息
+        AsyncManager.me().execute(new TimerTask() {
+            @Override
+            public void run() {
+                // 组装消息
+                Email email = new Email();
+                String[] tos = new String[]{};
+                tos = ArrayUtil.append(tos, userEmail);
+                email.setTos(tos);
+                email.setTitle("Git仓库认证添加通知");
+                email.setContent(userName + "添加" + gitAccess.getName() + "Git仓库认证，结果：" + (isSuccess ? "成功" : "失败"));
+            }
+        });
+        return isSuccess ? AjaxResult.success() : AjaxResult.error();
+    }
+
+    /**
+     * 修改
+     *
+     * @param gitAccess gitAccess
+     * @return r
+     */
+    @PutMapping("/edit")
+    @ApiOperation(value = "修改")
+    public AjaxResult edit(@RequestBody GitAccess gitAccess) {
+        // 获取邮箱
+        String userEmail = SecurityUtils.getLoginUser().getUser().getEmail();
+        // 获取用户名
+        String userName = SecurityUtils.getLoginUser().getUser().getUserName();
+        gitAccess.setUpdateBy(getUsername());
+        gitAccess.setUpdateTime(DateUtil.date());
+        boolean isSuccess = gitAccessService.updateById(gitAccess);
+        // 异步消息
+        AsyncManager.me().execute(new TimerTask() {
+            @Override
+            public void run() {
+                // 组装消息
+                Email email = new Email();
+                String[] tos = new String[]{};
+                tos = ArrayUtil.append(tos, userEmail);
+                email.setTos(tos);
+                email.setTitle("Git仓库认证修改通知");
+                email.setContent(userName + "修改" + gitAccess.getName() + "Git仓库认证，结果：" + (isSuccess ? "成功" : "失败"));
+            }
+        });
+        return isSuccess ? AjaxResult.success() : AjaxResult.error();
+    }
+
+    /**
+     * 删除
+     *
+     * @param id id
+     * @return r
+     */
+    @DeleteMapping("/delete")
+    @ApiOperation(value = "删除")
+    public AjaxResult delete(@RequestParam(value = "id") String id) {
+        // 获取邮箱
+        String userEmail = SecurityUtils.getLoginUser().getUser().getEmail();
+        // 获取用户名
+        String userName = SecurityUtils.getLoginUser().getUser().getUserName();
+        boolean isSuccess = gitAccessService.removeById(id);
+        // 异步消息
+        AsyncManager.me().execute(new TimerTask() {
+            @Override
+            public void run() {
+                // 获取GitAccess
+                GitAccess gitAccess = gitAccessService.getById(id);
+                // 组装消息
+                Email email = new Email();
+                String[] tos = new String[]{};
+                tos = ArrayUtil.append(tos, userEmail);
+                email.setTos(tos);
+                email.setTitle("Git仓库认证删除通知");
+                email.setContent(userName + "删除" + gitAccess.getName() + "Git仓库认证，结果：" + (isSuccess ? "成功" : "失败"));
+            }
+        });
+        return isSuccess ? AjaxResult.success() : AjaxResult.error();
+    }
+}
--- cloud-server-admin/src/main/java/vip/gpg123/git/domain/GitAccess.java
+++ cloud-server-admin/src/main/java/vip/gpg123/git/domain/GitAccess.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
@@ -18,20 +20,38 @@ public class GitAccess implements Serializable {
     /**
      * 
      */
-    @TableId(value = "id")
+    @TableId(value = "id",  type = IdType.ASSIGN_ID)
     private String id;
 
     /**
-     * 
+     * 名称
+     */
+    @TableField(value = "name")
+    private String name;
+
+    /**
+     * 类型
+     */
+    @TableField(value = "type")
+    private String type;
+
+    /**
+     * 认证方式
+     */
+    @TableField(value = "source")
+    private String source;
+
+    /**
+     * 主页
      */
     @TableField(value = "home_url")
-    private String home_url;
+    private String homeUrl;
 
     /**
      * token
      */
     @TableField(value = "access_token")
-    private String access_token;
+    private String accessToken;
 
     /**
      * 标签
@@ -49,25 +69,27 @@ public class GitAccess implements Serializable {
      * 
      */
     @TableField(value = "create_by")
-    private String create_by;
+    private String createBy;
 
     /**
      * 
      */
     @TableField(value = "create_time")
-    private Date create_time;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 
     /**
      * 
      */
     @TableField(value = "update_by")
-    private String update_by;
+    private String updateBy;
 
     /**
      * 
      */
     @TableField(value = "update_time")
-    private Date update_time;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
 
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
