543b342992fa5a42caf5936b203a7ef0f28ab64d
gaopuguang <2694484453@qq.com>
2025-05-09 18:59:56 +0800

    2025/05/08 repo

--- cloud-server-admin/src/main/java/vip/gpg123/git/domain/Git.java
+++ cloud-server-admin/src/main/java/vip/gpg123/git/domain/Git.java
@@ -23,72 +23,82 @@ public class Git implements Serializable {
     private Long id;
 
     /**
-     *
+     * 名称
      */
     private String name;
 
     /**
-     *
+     * 全名
+     */
+    private String fullName;
+
+    /**
+     * 归属
+     */
+    private String human_name;
+
+    /**
+     * 代号
      */
     private String code;
 
     /**
-     *
+     * 类型
      */
     private String type;
 
     /**
-     *
+     * 主页
      */
-    private String home;
+    private String homeUrl;
 
     /**
-     *
+     * http地址
      */
-    private String httpUrl;
+    private String htmlUrl;
 
     /**
-     *
+     * 仓库地址
+     */
+    private String gitUrl;
+
+    /**
+     * ssh地址
      */
     private String sshUrl;
 
     /**
-     *
-     */
-    private Boolean hasJob;
-
-    /**
-     *
+     * 语言
      */
     private String language;
 
     /**
-     *
+     * 状态
      */
-    private Object jobTypes;
+    private String status;
 
     /**
-     *
+     * 描述
      */
-    private Integer jobCount;
+    private String description;
 
     /**
-     *
+     * 创建者
      */
     private String createBy;
 
     /**
-     *
+     * 创建时间
      */
     private Date createTime;
 
     /**
-     *
+     * 更新者
      */
     private String updateBy;
 
     /**
-     *
+     * 更新时间
      */
     private Date updateTime;
 
