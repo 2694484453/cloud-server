62e58c5b65385dcdbf931eb2e30637ee82e027f1
gaopuguang <2694484453@qq.com>
2025-05-17 01:49:23 +0800

    2025/05/17 notice

--- cloud-server-admin/src/main/java/vip/gpg123/notice/SysActionNoticeController.java
+++ cloud-server-admin/src/main/java/vip/gpg123/notice/SysActionNoticeController.java
@@ -1,5 +1,7 @@
 package vip.gpg123.notice;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -7,10 +9,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vip.gpg123.common.core.controller.BaseController;
 import vip.gpg123.common.core.domain.AjaxResult;
 import vip.gpg123.common.core.page.TableDataInfo;
@@ -43,9 +42,11 @@ public class SysActionNoticeController extends BaseController {
                            @RequestParam(value = "title", required = false) String title) {
         List<SysActionNotice> list = sysActionNoticeService.list(new LambdaQueryWrapper<SysActionNotice>()
                 .eq(SysActionNotice::getCreateBy,  getUsername())
+                .eq(SysActionNotice::getIsConfirm,0)
                 .eq(StrUtil.isNotBlank(type),SysActionNotice::getType, type)
                 .eq(StrUtil.isNotBlank(sendType),SysActionNotice::getSendType, sendType)
                 .like(StrUtil.isNotBlank(title),SysActionNotice::getTitle, title)
+                .orderByDesc(SysActionNotice::getCreateTime)
         );
         return AjaxResult.success(list);
     }
@@ -71,4 +72,22 @@ public class SysActionNoticeController extends BaseController {
         return PageUtils.toPageByIPage(page);
     }
 
+    /**
+     * 设置已读
+     * @param id id
+     * @return r
+     */
+    @PutMapping("/setRead")
+    @ApiOperation(value = "【设置已读】")
+    public AjaxResult setRead(@RequestParam("id") String id) {
+        SysActionNotice search = sysActionNoticeService.getById(id);
+        if (ObjectUtil.isNull(search)) {
+            return error("查询不到此id数据");
+        }
+        search.setIsConfirm(1);
+        search.setUpdateBy(getUsername());
+        search.setUpdateTime(DateUtil.date());
+        boolean update = sysActionNoticeService.updateById(search);
+        return update ? success() : error();
+    }
 }
--- cloud-server-admin/src/main/java/vip/gpg123/notice/domain/SysActionNotice.java
+++ cloud-server-admin/src/main/java/vip/gpg123/notice/domain/SysActionNotice.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
@@ -79,7 +81,15 @@ public class SysActionNotice implements Serializable {
      * 创建时间
      */
     @TableField(value = "create_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @TableField(value = "update_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
 
     /**
      * 更新者
