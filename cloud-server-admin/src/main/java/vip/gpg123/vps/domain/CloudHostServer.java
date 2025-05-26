79f5222953eeef6334ee2f577d9ac8e4a79e7367
gaopuguang <2694484453@qq.com>
2025-05-26 23:26:26 +0800

    2025/05/26 nacos-config

--- cloud-server-admin/src/main/java/vip/gpg123/vps/domain/CloudHostServer.java
+++ cloud-server-admin/src/main/java/vip/gpg123/vps/domain/CloudHostServer.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
@@ -79,6 +81,7 @@ public class CloudHostServer implements Serializable {
      * 创建时间
      */
     @TableField(value = "create_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     /**
@@ -91,6 +94,7 @@ public class CloudHostServer implements Serializable {
      * 更新时间
      */
     @TableField(value = "update_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     @TableField(exist = false)
