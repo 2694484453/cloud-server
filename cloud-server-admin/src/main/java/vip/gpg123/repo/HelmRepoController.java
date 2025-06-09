832f566f3a01fb086583fe4575f327c186435767
gaopuguang <2694484453@qq.com>
2025-06-10 01:47:44 +0800

    2025/06/06 change

--- cloud-server-admin/src/main/java/vip/gpg123/repo/HelmRepoController.java
+++ cloud-server-admin/src/main/java/vip/gpg123/repo/HelmRepoController.java
@@ -1,10 +1,7 @@
 package vip.gpg123.repo;
 
-import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.setting.yaml.YamlUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
@@ -12,17 +9,13 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import vip.gpg123.common.core.controller.BaseController;
 import vip.gpg123.common.core.domain.AjaxResult;
 import vip.gpg123.common.core.page.TableDataInfo;
 import vip.gpg123.common.core.page.TableSupport;
 import vip.gpg123.common.utils.PageUtils;
-import vip.gpg123.common.utils.helm.HelmApp;
 import vip.gpg123.common.utils.helm.HelmUtils;
-import vip.gpg123.framework.manager.AsyncManager;
-import vip.gpg123.helm.domain.ChartApp;
 import vip.gpg123.repo.domain.HelmRepo;
 import vip.gpg123.repo.domain.HelmRepoItem;
 import vip.gpg123.repo.domain.HelmRepoResponse;
@@ -40,12 +33,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RestController
 @RequestMapping("/helmRepo")
 public class HelmRepoController extends BaseController {
-
-    @Value("${repo.helm.url}")
-    private String url;
-
-    @Value("${repo.helm.name}")
-    private String repoName;
 
     @Autowired
     private HelmRepoApiService helmRepoApiService;
@@ -103,8 +90,8 @@ public class HelmRepoController extends BaseController {
         HelmRepo helmRepo = helmRepoService.getOne(new LambdaQueryWrapper<HelmRepo>()
                 .eq(HelmRepo::getRepoName, repoName));
         // 查询到结果
-        AtomicInteger chartNum = new AtomicInteger(0);
-        AtomicInteger chartVersionNum = new AtomicInteger(0);
+        int chartNum = 0;
+        AtomicInteger chartVersionNum = new AtomicInteger();
         if (ObjectUtil.isNotNull(helmRepo)) {
             helmRepo.setStatus("updating");
             helmRepo.setUpdateTime(DateUtil.date());
@@ -112,63 +99,56 @@ public class HelmRepoController extends BaseController {
             boolean isSuccess;
             try {
                 isSuccess = HelmUtils.repoUpdate(repoName);
+
                 // 如果成功更新仓库数据
                 if (isSuccess) {
-                    // 执行异步数据
-                    AsyncManager.me().execute(new TimerTask() {
-                        @Override
-                        public void run() {
-                            // 请求接口
-                            String url = helmRepo.getRepoUrl();
-                            String apiUrl = url + "/index.yaml";
-                            // 请求仓库列表
-                            String res = helmRepoApiService.index(URI.create(apiUrl));
-                            HelmRepoResponse repoResponse = YamlUtil.load(StrUtil.getReader(res), HelmRepoResponse.class);
-                            Map<String, List<HelmRepoItem>> entries = repoResponse.getEntries();
-                            entries.forEach((k, v) -> {
-                                // 一个key就是一个应用，list数量即为版本数量
-                                chartNum.set(chartNum.get() + 1);
-                                chartVersionNum.set(chartVersionNum.get() + v.size());
-                            });
-                        }
+                    // 请求仓库列表
+                    String res = helmRepoApiService.index(URI.create(helmRepo.getRepoUrl()));
+                    HelmRepoResponse repoResponse = YamlUtil.load(StrUtil.getReader(res), HelmRepoResponse.class);
+                    Map<String, List<HelmRepoItem>> entries = repoResponse.getEntries();
+                    chartNum = entries.size();
+                    entries.forEach((k, v) -> {
+                        // 一个key就是一个应用，list数量即为版本数量
+                        chartVersionNum.set(chartVersionNum.get() + v.size());
                     });
                 }
             } catch (Exception e) {
                 helmRepo.setStatus("fail");
             } finally {
-                helmRepo.setArtifactTotal(chartNum.get());
+                helmRepo.setArtifactTotal(chartNum);
                 helmRepo.setArtifactVersionTotal(chartVersionNum.get());
                 helmRepo.setUpdateBy(getUsername());
                 helmRepo.setRepoUpdateTime(DateUtil.date());
                 helmRepoService.updateById(helmRepo);
             }
+            return AjaxResult.success("更新成功");
         }
         return AjaxResult.error("未查询到仓库");
     }
 
 
-    /**
-     * 查询详情
-     *
-     * @param name    名称
-     * @param version 版本
-     * @param type    类型
-     * @return r
-     */
-    @GetMapping("info")
-    @ApiOperation(value = "查看详情")
-    public AjaxResult info(@RequestParam(value = "name") String name,
-                           @RequestParam(value = "version", required = false) String version,
-                           @RequestParam(value = "type", required = false, defaultValue = "values") String type) {
-        String[] init = {"helm", "show"};
-        switch (type) {
-            case "values":
-                init = ArrayUtil.append(init, "values", repoName + "/" + name);
-                break;
-            case "readme":
-                init = ArrayUtil.append(init, "readme", repoName + "/" + name);
-        }
-        String res = RuntimeUtil.execForStr(init);
-        return null;
-    }
+//    /**
+//     * 查询详情
+//     *
+//     * @param name    名称
+//     * @param version 版本
+//     * @param type    类型
+//     * @return r
+//     */
+//    @GetMapping("info")
+//    @ApiOperation(value = "查看详情")
+//    public AjaxResult info(@RequestParam(value = "name") String name,
+//                           @RequestParam(value = "version", required = false) String version,
+//                           @RequestParam(value = "type", required = false, defaultValue = "values") String type) {
+//        String[] init = {"helm", "show"};
+//        switch (type) {
+//            case "values":
+//                init = ArrayUtil.append(init, "values", repoName + "/" + name);
+//                break;
+//            case "readme":
+//                init = ArrayUtil.append(init, "readme", repoName + "/" + name);
+//        }
+//        String res = RuntimeUtil.execForStr(init);
+//        return null;
+//    }
 }
--- cloud-server-admin/src/main/java/vip/gpg123/repo/domain/HelmRepo.java
+++ cloud-server-admin/src/main/java/vip/gpg123/repo/domain/HelmRepo.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
@@ -67,12 +69,14 @@ public class HelmRepo implements Serializable {
      * 仓库更新时间
      */
     @TableField(value = "repo_update_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date repoUpdateTime;
 
     /**
      * 创建时间
      */
     @TableField(value = "create_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     /**
@@ -91,6 +95,7 @@ public class HelmRepo implements Serializable {
      * 更新时间
      */
     @TableField(value = "update_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     @TableField(exist = false)
