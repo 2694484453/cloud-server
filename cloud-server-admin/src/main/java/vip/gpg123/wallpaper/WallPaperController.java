6f3ff3b9142649ee134aa21e877ad9d45a6e9849
gaopuguang <2694484453@qq.com>
2025-10-28 00:30:46 +0800

    2025/10/28 oss

---
+++ cloud-server-admin/src/main/java/vip/gpg123/wallpaper/WallPaperController.java
@@ -0,0 +1,51 @@
+package vip.gpg123.wallpaper;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.ListObjectsV2Result;
+import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyuncs.exceptions.ClientException;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wallpaper")
+public class WallPaperController {
+
+    @Autowired
+    private OSS ossClient;
+
+    private static final String bucketName= "dev-gpg";
+
+    private static final String keyPrefix = "image";
+
+    @GetMapping("/list")
+    public String list(){
+        try {
+            // 列举文件。如果不设置keyPrefix，则列举存储空间下的所有文件。如果设置keyPrefix，则列举包含指定前缀的文件。
+            ListObjectsV2Result result = ossClient.listObjectsV2(bucketName, keyPrefix);
+            List<OSSObjectSummary> ossObjectSummaries = result.getObjectSummaries();
+
+            for (OSSObjectSummary s : ossObjectSummaries) {
+                System.out.println("\t" + s.getKey());
+            }
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        return "<UNK>";
+    }
+}
