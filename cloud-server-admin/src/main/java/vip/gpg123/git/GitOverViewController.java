39dfc279b0525276e575cfea2a49033159d522e4
gaopuguang <2694484453@qq.com>
2025-11-25 17:54:19 +0800

    调整

--- cloud-server-admin/src/main/java/vip/gpg123/git/GitOverViewController.java
+++ cloud-server-admin/src/main/java/vip/gpg123/git/GitOverViewController.java
@@ -19,8 +19,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static vip.gpg123.common.utils.SecurityUtils.getUsername;
-
 /**
  * @author gaopuguang_zz
  * @version 1.0
