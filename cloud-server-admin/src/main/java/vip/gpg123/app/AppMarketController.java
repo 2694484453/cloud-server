3a87ec73033c1714fca34bbaeb4a53116a4d13a2
gaopuguang <2694484453@qq.com>
2025-12-12 14:03:55 +0800

    调整

--- cloud-server-admin/src/main/java/vip/gpg123/app/AppMarketController.java
+++ cloud-server-admin/src/main/java/vip/gpg123/app/AppMarketController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.Yaml;
 import vip.gpg123.app.domain.HelmAppMarket;
 import vip.gpg123.app.domain.IndexResponse;
 import vip.gpg123.app.domain.MineApp;
--- cloud-server-admin/src/main/java/vip/gpg123/app/AppOverViewController.java
+++ cloud-server-admin/src/main/java/vip/gpg123/app/AppOverViewController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vip.gpg123.app.domain.MineApp;
+import vip.gpg123.app.service.HelmAppMarketService;
 import vip.gpg123.app.service.MineAppService;
 import vip.gpg123.common.core.controller.BaseController;
 import vip.gpg123.common.core.domain.AjaxResult;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,6 +27,9 @@ public class AppOverViewController extends BaseController {
     @Autowired
     private MineAppService mineAppService;
 
+    @Autowired
+    private HelmAppMarketService helmAppMarketService;
+
     /**
      * 应用概览
      * @return r
@@ -30,12 +37,20 @@ public class AppOverViewController extends BaseController {
     @GetMapping("/overView")
     @ApiOperation(value = "应用概览")
     public AjaxResult overView() {
+        List<Map<String,Object>> list = new ArrayList<>();
+        Map<String,Object> appMarket = new HashMap<>();
+        appMarket.put("title","应用市场应用总数");
+        appMarket.put("count",helmAppMarketService.count());
+        list.add(appMarket);
+
         Map<String,Object> map = new LinkedHashMap<>();
         // 应用总数量
-        map.put("appTotalCount", mineAppService.count(new LambdaQueryWrapper<MineApp>()
-                .eq(MineApp::getCreateBy,  getUsername())
+        map.put("title", "我的应用总数量");
+        map.put("count", mineAppService.count(new LambdaQueryWrapper<MineApp>()
+                .eq(MineApp::getCreateBy,  getUserId())
         ));
-        return AjaxResult.success(map);
+        list.add(map);
+        return AjaxResult.success(list);
     }
 
 }
