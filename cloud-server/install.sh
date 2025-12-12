86a7692df44342102aa532f3ddec5634e1b859e9
gaopuguang <2694484453@qq.com>
2025-12-12 13:53:37 +0800

    调整

--- cloud-server/install.sh
+++ cloud-server/install.sh
@@ -1,3 +1,3 @@
 helm install cloud-server ../cloud-server -n cloud-server
 
-helm upgrade cloud-server ../cloud-server -n cloud-server
+helm upgrade cloud-server ./cloud-server -n cloud-server --kube-context=hcs.gpg123.vip
